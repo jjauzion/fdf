e67d73299d61cb6b7fcc8dc2d3d0c301a1630bfa
jjauzion <jjauzion@student.42.fr>
2018-04-16 18:39:40 +0200

    i and o button reveresed

--- src/print2screen.c
+++ src/print2screen.c
@@ -6,7 +6,7 @@
 /*   By: jjauzion <jjauzion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/21 15:15:58 by jjauzion          #+#    #+#             */
-/*   Updated: 2018/03/01 16:28:05 by jjauzion         ###   ########.fr       */
+/*   Updated: 2018/04/16 18:38:10 by jjauzion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,8 @@ static void	keyhandler(t_data *data, int key)
 	}
 	if (key == 8)
 		data->option = (data->option == 'c') ? '\0' : 'c';
-	(key == 78 || key == 34) ? data->height_factor-- : 0;
-	(key == 69 || key == 31) ? data->height_factor++ : 0;
+	(key == 78 || key == 31) ? data->height_factor-- : 0;
+	(key == 69 || key == 34) ? data->height_factor++ : 0;
 	data->translation[0] += (key == 123) ? -DEF_SHIFT : 0;
 	data->translation[0] += (key == 124) ? DEF_SHIFT : 0;
 	data->translation[1] += (key == 125) ? DEF_SHIFT : 0;
